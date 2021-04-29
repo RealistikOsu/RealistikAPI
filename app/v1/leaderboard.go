7ca907110d4ddf8d2204446e08d47f50b1955bb5
lenforiee <lenforiee@gmail.com>
2021-04-29 12:51:35 +0200

    implementing score api lbs

--- app/v1/leaderboard.go
+++ app/v1/leaderboard.go
@@ -37,9 +37,7 @@ const rxUserQuery = `
 			rx_stats.avg_accuracy_%[1]s, rx_stats.pp_%[1]s
 		FROM users
 		INNER JOIN rx_stats ON rx_stats.id = users.id
-		INNER JOIN users_stats ON users_stats.id = users.id
-		WHERE users.id IN (?)
-		`
+		INNER JOIN users_stats ON users_stats.id = users.id `
 
 const apUserQuery = `
 		SELECT
@@ -53,9 +51,7 @@ const apUserQuery = `
 			ap_stats.avg_accuracy_%[1]s, ap_stats.pp_%[1]s
 		FROM users
 		INNER JOIN ap_stats ON ap_stats.id = users.id
-		INNER JOIN users_stats ON users_stats.id = users.id
-		WHERE users.id IN (?)
-		`
+		INNER JOIN users_stats ON users_stats.id = users.id `
 
 const lbUserQuery = `
 		SELECT
@@ -68,9 +64,56 @@ const lbUserQuery = `
 			users_stats.replays_watched_%[1]s, users_stats.total_hits_%[1]s,
 			users_stats.avg_accuracy_%[1]s, users_stats.pp_%[1]s
 		FROM users
-		INNER JOIN users_stats ON users_stats.id = users.id
-		WHERE users.id IN (?)
-		`
+		INNER JOIN users_stats ON users_stats.id = users.id `
+
+func getScoreLb(m, rx, p, l int, country, sorted string, md *common.MethodData) []leaderboardUser {
+	var query, order, whereClause string
+
+	if country != "" {
+		country = strings.ToUpper(country)
+		whereClause = fmt.Sprintf(" AND users.country = %s", country)
+	}
+
+	switch rx {
+	case 1:
+		order = " ORDER BY rx_stats.ranked_score_%[1]s DESC, rx_stats.pp_%[1]s DESC"
+		query = fmt.Sprintf(rxUserQuery+"WHERE (users.privileges & 3) >= 3"+whereClause+order+" LIMIT %d, %d", m, p*l, l)
+	case 2:
+		order = " ORDER BY ap_stats.ranked_score_%[1]s DESC, ap_stats.pp_%[1]s DESC"
+		query = fmt.Sprintf(apUserQuery+"WHERE (users.privileges & 3) >= 3"+whereClause+order+" LIMIT %d, %d", m, p*l, l)
+	default:
+		order = " ORDER BY users_stats.ranked_score_%[1]s DESC, users_stats.pp_%[1]s DESC"
+		query = fmt.Sprintf(lbUserQuery+"WHERE (users.privileges & 3) >= 3"+whereClause+order+" LIMIT %d, %d", m, p*l, l)
+	}
+
+	rows, err := md.DB.Query(query)
+	if err != nil {
+		md.Err(err)
+		return make([]leaderboardUser, 0)
+	}
+	defer rows.Close()
+	var users []leaderboardUser
+	for i := 1; rows.Next(); i++ {
+		u := leaderboardUser{}
+		err := rows.Scan(
+			&u.ID, &u.Username, &u.RegisteredOn, &u.Privileges, &u.LatestActivity,
+
+			&u.UsernameAKA, &u.Country, &u.PlayStyle, &u.FavouriteMode,
+
+			&u.ChosenMode.RankedScore, &u.ChosenMode.TotalScore, &u.ChosenMode.PlayCount,
+			&u.ChosenMode.ReplaysWatched, &u.ChosenMode.TotalHits,
+			&u.ChosenMode.Accuracy, &u.ChosenMode.PP,
+		)
+		if err != nil {
+			md.Err(err)
+			continue
+		}
+		u.ChosenMode.Level = ocl.GetLevelPrecise(int64(u.ChosenMode.TotalScore))
+		users = append(users, u)
+	}
+	return users
+
+}
 
 // LeaderboardGET gets the leaderboard.
 func LeaderboardGET(md common.MethodData) common.CodeMessager {
@@ -82,6 +125,7 @@ func LeaderboardGET(md common.MethodData) common.CodeMessager {
 		p = 0
 	}
 	l := common.InString(1, md.Query("l"), 500, 50)
+	sorted := md.Query("sort")
 
 	key := "ripple:leaderboard:" + m
 	if common.Int(md.Query("rx")) == 1 {
@@ -92,6 +136,16 @@ func LeaderboardGET(md common.MethodData) common.CodeMessager {
 	}
 	if md.Query("country") != "" {
 		key += ":" + md.Query("country")
+		country := md.Query("country")
+	}
+	if sorted == "" {
+		sorted = "pp"
+	}
+
+	if sorted == "score" {
+		response = leaderboardResponse{Users: getScoreLb(m, rx, p, l, country, sorted, &md)}
+		response.Code = 200
+		return response
 	}
 
 	results, err := md.R.ZRevRange(key, int64(p*l), int64(p*l+l-1)).Result()
@@ -107,12 +161,12 @@ func LeaderboardGET(md common.MethodData) common.CodeMessager {
 		return resp
 	}
 
-	query := fmt.Sprintf(lbUserQuery+` ORDER BY users_stats.pp_%[1]s DESC, users_stats.ranked_score_%[1]s DESC`, m)
+	query := fmt.Sprintf(lbUserQuery+`WHERE users.id IN (?) ORDER BY users_stats.pp_%[1]s DESC, users_stats.ranked_score_%[1]s DESC`, m)
 	if common.Int(md.Query("rx")) == 1 {
-		query = fmt.Sprintf(rxUserQuery+` ORDER BY rx_stats.pp_%[1]s DESC, rx_stats.ranked_score_%[1]s DESC`, m)
+		query = fmt.Sprintf(rxUserQuery+`WHERE users.id IN (?) ORDER BY rx_stats.pp_%[1]s DESC, rx_stats.ranked_score_%[1]s DESC`, m)
 	}
 	if common.Int(md.Query("rx")) == 2 {
-		query = fmt.Sprintf(apUserQuery+` ORDER BY ap_stats.pp_%[1]s DESC, ap_stats.ranked_score_%[1]s DESC`, m)
+		query = fmt.Sprintf(apUserQuery+`WHERE users.id IN (?) ORDER BY ap_stats.pp_%[1]s DESC, ap_stats.ranked_score_%[1]s DESC`, m)
 	}
 	query, params, _ := sqlx.In(query, results)
 	rows, err := md.DB.Query(query, params...)
