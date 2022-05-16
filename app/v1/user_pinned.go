23ab9bc6c76a9a2485ac15836b5ae511f2164699
Kaimi8 <kai.tech22@gmail.com>
2022-05-16 03:03:21 +0000

    feat: get user's pinned

--- app/v1/user_pinned.go
+++ app/v1/user_pinned.go
@@ -2,41 +2,87 @@ package v1
 
 import (
 	"database/sql"
+	"fmt"
 	"time"
 
 	"github.com/RealistikOsu/RealistikAPI/common"
 )
 
-type pinnedScore struct {
-	common.ResponseBase
-	Scores []struct {
-		Score
+func UserPinnedGET(md common.MethodData) common.CodeMessager {
+	scoreResponse := userScoresResponse{}
+	dbs := []string{"", "_relax", "_ap"}
 
-		Beatmap beatmap `json:"beatmap"`
-	} `json:"scores"`
-}
+	playmode := common.Int(md.Query("playmode"))
+	mode := common.Int(md.Query("mode"))
+	userid := common.Int(md.Query("id"))
 
-const pinnedQuery = `
-SELECT
-    scores.id, scores.beatmap_md5, scores.score,
-    scores.max_combo, scores.full_combo, scores.mods,
-    scores.300_count, scores.100_count, scores.50_count,
-    scores.gekis_count, scores.katus_count, scores.misses_count,
-    scores.time, scores.play_mode, scores.accuracy, scores.pp,
-    scores.completed, users_pinned.pin_date
-    beatmaps.beatmap_id, beatmaps.beatmapset_id, beatmaps.beatmap_md5,
-    beatmaps.song_name, beatmaps.ar, beatmaps.od, beatmaps.difficulty_std,
-    beatmaps.difficulty_taiko, beatmaps.difficulty_ctb, beatmaps.difficulty_mania,
-    beatmaps.max_combo, beatmaps.hit_length, beatmaps.ranked,
-    beatmaps.ranked_status_freezed, beatmaps.latest_update
-FROM user_pinned
-JOIN scores ON scores.id = scoreid
-JOIN beatmaps on scores.beatmap_md5 = beatmaps.beatmap_md5
-WHERE user_pinned.userid = 1000 AND scores.mode = 1
-`
+	if playmode < 0 || playmode > 2 {
+		return common.SimpleResponse(400, "Invalid playmode")
+	}
 
-func UserPinnedGET() {
-	panic("unimplemented")
+	if mode < 0 || mode > 3 {
+		return common.SimpleResponse(400, "Invalid mode")
+	}
+
+	query := fmt.Sprintf(`
+		SELECT
+			scores%[1]s.id, scores%[1]s.beatmap_md5, scores%[1]s.score,
+			scores%[1]s.max_combo, scores%[1]s.full_combo, scores%[1]s.mods,
+			scores%[1]s.300_count, scores%[1]s.100_count, scores%[1]s.50_count,
+			scores%[1]s.gekis_count, scores%[1]s.katus_count, scores%[1]s.misses_count,
+			scores%[1]s.time, scores%[1]s.play_mode, scores%[1]s.accuracy, scores%[1]s.pp,
+			scores%[1]s.completed,
+
+			beatmaps.beatmap_id, beatmaps.beatmapset_id, beatmaps.beatmap_md5,
+			beatmaps.song_name, beatmaps.ar, beatmaps.od, beatmaps.difficulty_std,
+			beatmaps.difficulty_taiko, beatmaps.difficulty_ctb, beatmaps.difficulty_mania,
+			beatmaps.max_combo, beatmaps.hit_length, beatmaps.ranked,
+			beatmaps.ranked_status_freezed, beatmaps.latest_update
+		FROM user_pinned
+		JOIN scores%[1]s ON scores%[1]s.id = scoreid
+		JOIN beatmaps on scores%[1]s.beatmap_md5 = beatmaps.beatmap_md5
+		WHERE user_pinned.userid = ? AND scores%[1]s.play_mode = ?
+	`, dbs[playmode]) + common.Paginate(md.Query("p"), md.Query("l"), 100)
+
+	rows, err := md.DB.Query(query, userid, mode)
+
+	if err != nil {
+		md.Err(err)
+		return Err500
+	}
+
+	for rows.Next() {
+		var (
+			us userScore
+			b  beatmap
+		)
+
+		err = rows.Scan(
+			&us.ID, &us.BeatmapMD5, &us.Score.Score,
+			&us.MaxCombo, &us.FullCombo, &us.Mods,
+			&us.Count300, &us.Count100, &us.Count50,
+			&us.CountGeki, &us.CountKatu, &us.CountMiss,
+			&us.Time, &us.PlayMode, &us.Accuracy, &us.PP,
+			&us.Completed,
+
+			&b.BeatmapID, &b.BeatmapsetID, &b.BeatmapMD5,
+			&b.SongName, &b.AR, &b.OD, &b.Diff2.STD,
+			&b.Diff2.Taiko, &b.Diff2.CTB, &b.Diff2.Mania,
+			&b.MaxCombo, &b.HitLength, &b.Ranked,
+			&b.RankedStatusFrozen, &b.LatestUpdate,
+		)
+
+		if err != nil {
+			md.Err(err)
+			return Err500
+		}
+
+		us.Beatmap = b
+		scoreResponse.Scores = append(scoreResponse.Scores, us)
+	}
+
+	scoreResponse.Code = 200
+	return scoreResponse
 }
 
 func UserUnpinDELETE() {
@@ -56,7 +102,7 @@ func UserPinnedPOST(md common.MethodData) common.CodeMessager {
 	}
 
 	if mode < 0 || mode > 2 {
-		return common.SimpleResponse(400, "Invalid mode")
+		return common.SimpleResponse(400, "Invalid playmode")
 	}
 
 	res := common.ResponseBase{}
