4515810bc407a43eab16c05b3516d8b0ba8699f9
Kaimi8 <kai.tech22@gmail.com>
2022-05-23 06:00:28 +0000

    feat: get comments

--- app/v1/comments.go
+++ app/v1/comments.go
@@ -9,12 +9,26 @@ import (
 )
 
 // TODO: Allow users to disable comments (through settings and admin panel)
+// TODO: Let admin bypass disable_comments
 // TODO? Profanity check
 
 const (
 	MAX_LENGTH = 380
 	MIN_LENGTH = 3
 )
+
+type comment struct {
+	Op       int    `json:"op"`
+	UserID     int    `json:"userid"`
+	Username     string    `json:"username"`
+	Message  string `json:"message"`
+	PostedAt int64  `json:"posted_at"`
+}
+
+type comments struct {
+	common.ResponseBase
+	Comments []comment `json:"comments"`
+}
 
 func CommentPOST(md common.MethodData) common.CodeMessager {
 	var commentDate int64 = time.Now().Unix()
@@ -27,21 +41,19 @@ func CommentPOST(md common.MethodData) common.CodeMessager {
 	comment := string(md.Ctx.Request.Body())
 	op := md.User.UserID
 
-	// is user restricted?
+	// is user restricted? am i restricted? do they allow comments?
 	err := md.DB.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE privileges & 1 = 1 AND id = ?);", userid).Scan(&userExists)
 	if err != nil && err != sql.ErrNoRows {
 		md.Err(err)
 		return Err500
 	}
 
-	// am i restricted?
 	err = md.DB.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE privileges & 1 = 1 AND id = ?);", op).Scan(&doIExist)
 	if err != nil && err != sql.ErrNoRows {
 		md.Err(err)
 		return Err500
 	}
 
-	// does user allow comments?
 	err = md.DB.QueryRow("SELECT disabled_comments FROM users WHERE id = ?;", userid).Scan(&canComment)
 	if err != nil && err != sql.ErrNoRows {
 		md.Err(err)
@@ -49,21 +61,11 @@ func CommentPOST(md common.MethodData) common.CodeMessager {
 	}
 
 	if !userExists || !doIExist {
-		res.Code = 403
-		res.Message = "You don't have the permissions to carry out this action!"
-		return res
-	}
-
-	if canComment == 1 {
-		res.Code = 403
-		res.Message = "This user has disabled comments on their profile."
-		return res
-	}
-
-	if len(comment) > MAX_LENGTH || len(comment) < MIN_LENGTH {
-		res.Code = 400
-		res.Message = fmt.Sprintf("Invalid comment! Comment must be between %d and %d in length.", MIN_LENGTH, MAX_LENGTH)
-		return res
+		return common.SimpleResponse(403, "You don't have the permissions to carry out this action!")
+	} else if canComment == 1 {
+		return common.SimpleResponse(403, "This user has disabled comments on their profile.")
+	} else if len(comment) > MAX_LENGTH || len(comment) < MIN_LENGTH {
+		return common.SimpleResponse(400, fmt.Sprintf("Invalid comment! Comment must be between %d and %d in length.", MIN_LENGTH, MAX_LENGTH))
 	}
 
 	_, err = md.DB.Exec("INSERT INTO user_comments (op, prof, msg, comment_date) VALUES (?, ?, ?, ?)", op, userid, comment, commentDate)
@@ -77,8 +79,66 @@ func CommentPOST(md common.MethodData) common.CodeMessager {
 	return res
 }
 
-func CommentGET() {
-	panic("unimplemented")
+func CommentGET(md common.MethodData) common.CodeMessager {
+	var commentsAllowed int
+	var userExists bool
+
+	res := comments{}
+	userid := common.Int(md.Query("id"))
+	err := md.DB.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE privileges & 1 = 1 AND id = ?);", userid).Scan(&userExists)
+
+	if err != nil && err != sql.ErrNoRows {
+		md.Err(err)
+		return Err500
+	}
+
+	err = md.DB.QueryRow("SELECT disabled_comments FROM users WHERE id = ?;", userid).Scan(&commentsAllowed)
+	if err != nil && err != sql.ErrNoRows {
+		md.Err(err)
+		return Err500
+	}
+
+	if !userExists || commentsAllowed == 1 {
+		return common.SimpleResponse(404, "Profile not found/comments are disabled.")
+	}
+
+	cquery := `
+		SELECT
+			user_comments.op, user_comments.prof,
+			user_comments.msg, user_comments.comment_date,
+			users.username
+		FROM user_comments
+		INNER JOIN users ON users.id = user_comments.op
+		WHERE user_comments.prof = ? AND users.privileges & 1 = 1
+		ORDER BY user_comments.comment_date DESC
+	` + common.Paginate(md.Query("p"), md.Query("l"), 5)
+
+	rows, err := md.DB.Query(cquery, userid)
+
+	if err != nil {
+		md.Err(err)
+		return Err500
+	}
+
+	for rows.Next() {
+		cmt := comment{}
+
+		err = rows.Scan(
+			&cmt.Op, &cmt.UserID,
+			&cmt.Message, &cmt.PostedAt,
+			&cmt.Username,
+		)
+
+		if err != nil {
+			md.Err(err)
+			return Err500
+		}
+
+		res.Comments = append(res.Comments, cmt)
+	}
+
+	res.Code = 200
+	return res
 }
 
 func CommentDELETE() {
